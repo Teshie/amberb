345055507576208c1632d8a7d1daabd85f9c657a
Teshie <teshomeaychiluhem@gmail.com>
2026-03-12 10:49:24 +0300

    fix

--- bot.go
+++ bot.go
@@ -169,26 +169,26 @@ func desiredBotCount(roomID string, now time.Time) int {
 
 	switch {
 	case totalMin >= 5*60 && totalMin < 6*60:
-		base = triple{400, 0, 0}
+		base = triple{170, 0, 0}
 
 	case totalMin >= 6*60 && totalMin < 12*60: // 06:00–12:00
-		base = triple{400, 0, 0}
+		base = triple{170, 0, 0}
 
 	case totalMin >= 12*60 && totalMin < 15*60: // 12:00–15:00
-		base = triple{400, 0, 0}
+		base = triple{170, 0, 0}
 
 	case totalMin >= 15*60 && totalMin < 17*60: // 15:00–17:00
-		base = triple{400, 0, 0}
+		base = triple{170, 0, 0}
 
 	case totalMin >= 17*60 && totalMin < (23*60+30): // 17:00–23:30 → evening peak of
-		base = triple{400, 0, 0}
+		base = triple{170, 0, 0}
 
 	case totalMin >= (23*60+30) && totalMin < (24*60+30): // 23:30–00:30 → decline phase
 		minutesFrom2330 := totalMin - (23*60 + 30)
 		totalWindow := (24*60 + 30) - (23*60 + 30) // 60 minutes
 
 		if totalWindow <= 0 {
-			base = triple{400, 0, 0}
+			base = triple{170, 0, 0}
 			break
 		}
 
@@ -198,7 +198,7 @@ func desiredBotCount(roomID string, now time.Time) int {
 			frac = 0
 		}
 
-		peak := triple{400, 0, 0} // your chosen late-night peak
+		peak := triple{170, 0, 0} // your chosen late-night peak
 		base = triple{
 			int(frac * float64(peak.r10)),
 			int(frac * float64(peak.r20)),
